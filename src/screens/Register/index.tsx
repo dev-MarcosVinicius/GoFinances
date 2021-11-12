e0a00d7af861f799cdcd97565e44b01487d30e95
marcos.oliveira6 <marcosoliveira.ti9@gmail.com>
2021-11-12 14:25:36 -0300

    Implementação do formulario

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
+
+import { 
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionTypes
+} from './styles'
+
+export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Title>
+                    Cadastro
+                </Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+
+                    <Input
+                        placeholder="Preço"
+                    />
+
+                    <TransactionTypes>
+                        <TransactionTypeButton 
+                            title="Income"
+                            type="up"
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton 
+                            title="Outcome"
+                            type="down"
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionTypes>
+
+                    <CategorySelect title="Categoria"/>
+                </Fields>
+                <Button title="Enviar"/>
+            </Form>
+        </Container>
+    )
+}
